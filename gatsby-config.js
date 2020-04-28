09f2b35e82ae808bd5f6f85f2b97e005b79f8c67
Fabrizio Ferri-Benedetti <fferribenedetti@newrelic.com>
2020-04-28 10:51:50 +0200

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: 'New Relic Opendocs Theme',
+    description: 'A Gatsby theme for New Relic documentation'
+  },
+  pathPrefix: "/nr-oss-theme",
+  plugins: [
+    'gatsby-theme-documentation',
+    'gatsby-plugin-sass'
+  ]
+}
